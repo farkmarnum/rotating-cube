1b34394a51a05ad4b5ee6f45adf25d103abeff77
Mark Farnum <mark.farnum@propagateag.com>
2024-06-07 20:05:31 -0400

    wip

---
+++ README.md
@@ -0,0 +1,18 @@
+# notes
+
+- figure out where the vertexes are
+- figure out where the lines are
+- project the camera
+
+how do you draw a line on a pixel grid?
+
+---
+
+NOTE:
+- "pixel" coordinates are integers, `0` to `WIDTH-1` and `0` to `HEIGHT-1`
+  `px, py`
+- "point" coordinates are floats, `0.0` to `1.0`
+  `x, y`
+- "vertex" coordinates are floats, unbounded
+  `vx, vy`
+
--- game.js
+++ game.js
@@ -15,25 +15,90 @@ const cleanup = () => {
   process.stderr.write("\x1B[?25h"); // Show cursor
 };
 
+// screen dimensions
 const WIDTH = 30;
 const HEIGHT = 15;
 
-// quit gracefully
-process.on("SIGINT", () => {
-  cleanup();
-  process.exit();
-});
+const pointToPixel = ([x, y]) => {
+  return [Math.round(x * (WIDTH - 1)), Math.round(y * (HEIGHT - 1))];
+};
+
+const distance = ([x0, y0], [x1, y1]) => (x1 - x0) ** 2 + (y1 - y0) ** 2;
 
 const run = async () => {
-  const lines = [[0.25, 0.25, 0.25, 0.75]];
+  const lines = [[0.25, 0.25, 0.5, 0.5]];
 
-  const checkPixel = (x, y) => {
-    return lines.some((line) => {
-      const [x0, y0, x1, y1] = line;
-      const diff = Math.abs(((y1 - y0) * (x - x0)) / (x1 - x0) - y);
-      log(diff);
-      return diff < 0.5;
-    });
+  // buffer for screen pixel values
+  const screen = new Array(HEIGHT).fill().map(() => new Array(WIDTH).fill(0));
+
+  const drawLine = (line) => {
+    // point coords
+    const [x0, y0, x1, y1] = line;
+
+    // pixel coords:
+    const [px0, py0] = pointToPixel([x0, y0]);
+    const [px1, py1] = pointToPixel([x1, y1]);
+
+    // the directions we'll be moving
+    const dx = px1 === px0 ? 0 : px1 > px0 ? 1 : -1;
+    const dy = py1 === py0 ? 0 : py1 > py0 ? 1 : -1;
+
+    // current pixel
+    let px = px0;
+    let py = py0;
+
+    // console.log("start");
+    // console.log({ px0, py0 });
+    // console.log({ px1, py1 });
+    // console.log({ px, py });
+    // console.log({ dx, dy });
+
+    // draw the first point
+    screen[py][px] = 1;
+
+    let i = 0; // safety valve
+    let slopeIfWeChangeX;
+    let slopeIfWeChangeY;
+    let slopeOfLine;
+    let xSlopeDiff;
+    let ySlopeDiff;
+    console.log({ px0, py0 });
+    console.log({ px1, py1 });
+    try {
+      while (px !== px1 || py !== py1) {
+        if (i++ > 100) {
+          console.log();
+          console.log(JSON.stringify({ px, py, px0, py0, px1, py1 }));
+          throw new Error("max loop exceeded!");
+        }
+
+        // figure where to go next, either [px + dx, py] or [px, py + dy]
+        // for each point, we'll calculate the slope of line from it to [px1, py1]
+        // and we'll pick the one whose slope is closer to the actual slope
+        slopeIfWeChangeX = (py1 - py) / (px1 - (px + dx));
+        slopeIfWeChangeY = (py1 - (py + dy)) / (px1 - px);
+        slopeOfLine = (py1 - py0) / (px0 - px1);
+        xSlopeDiff = Math.abs(slopeIfWeChangeX - slopeOfLine);
+        ySlopeDiff = Math.abs(slopeIfWeChangeY - slopeOfLine);
+
+        if (xSlopeDiff <= ySlopeDiff) px += dx;
+        if (xSlopeDiff >= ySlopeDiff) py += dy;
+
+        console.log({ px, py });
+
+        // draw the point
+        screen[py][px] = 1;
+        // console.log(py, px);
+      }
+    } catch {
+      console.log({
+        slopeIfWeChangeX,
+        slopeIfWeChangeY,
+        slopeOfLine,
+        xSlopeDiff,
+        ySlopeDiff,
+      });
+    }
   };
 
   try {
@@ -43,23 +108,29 @@ const run = async () => {
       // start by clearing the screen
       console.clear();
 
+      // draw the lines
+      lines.forEach((line) => drawLine(line));
+
       // render the pixels
       for (let i = 0; i < HEIGHT; i++) {
         for (let j = 0; j < WIDTH; j++) {
-          const x = j / (WIDTH - 1);
-          const y = i / (HEIGHT - 1);
-          p(checkPixel(x, y) ? "#" : "_");
+          p(screen[i][j] ? "#" : "_");
         }
         p("\n");
       }
 
-      throw new Error("quit");
       // wait for next frame
-      // await sleep(1000 / 60); // 60 fps
+      await sleep(1000 / 60); // 60 fps
     }
   } finally {
     cleanup();
   }
 };
+
+// quit gracefully
+process.on("SIGINT", () => {
+  cleanup();
+  process.exit();
+});
 
 run();
